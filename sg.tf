9649461997f00e118be377b27f788b5d8466a78d
Sajath Firthows <106432527+sajathfirthows@users.noreply.github.com>
2024-02-25 00:44:31 +0530

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -1,23 +1,22 @@
-# create security group for the web server
-# terraform aws create security group
+# securitygrp.tf
+
 resource "aws_security_group" "efs" {
   name        = "my-server-sg"
   description = "enable http/https access on port 80/443 via alb sg and access on port 22 via ssh sg"
   vpc_id      = aws_vpc.my_vpc.id
 
   ingress {
-    description      = "ssh access"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "ssh access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
- 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = -1
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
