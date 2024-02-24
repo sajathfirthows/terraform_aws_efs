a72ce32ea27f0e5459ea1fe757e4b615a95f6ca7
Sajath Firthows <106432527+sajathfirthows@users.noreply.github.com>
2024-02-25 00:43:47 +0530

    Update efs.tf

--- efs.tf
+++ efs.tf
@@ -1,16 +1,26 @@
+# efs.tf
+
 resource "aws_efs_file_system" "efs" {
-   creation_token = "efs"
-   performance_mode = "generalPurpose"
-   throughput_mode = "bursting"
-   encrypted = "true"
- tags = {
-     Name = "EFS"
-   }
- }
+  creation_token   = "efs"
+  performance_mode = "generalPurpose"
+  throughput_mode  = "bursting"
+  encrypted        = true
 
+  tags = {
+    Name = "EFS"
+  }
+}
 
-resource "aws_efs_mount_target" "efs-mt" {
-   file_system_id  = aws_efs_file_system.efs.id
-   subnet_id = aws_subnet.subnet_1.id
-   security_groups = [aws_security_group.efs.id]
- }
+resource "aws_efs_mount_target" "efs-mt-1" {
+  count            = 1
+  file_system_id   = aws_efs_file_system.efs.id
+  subnet_id        = aws_subnet.subnet_1.id
+  security_groups = [aws_security_group.efs.id]
+}
+
+resource "aws_efs_mount_target" "efs-mt-2" {
+  count            = 1
+  file_system_id   = aws_efs_file_system.efs.id
+  subnet_id        = aws_subnet.subnet_2.id
+  security_groups = [aws_security_group.efs.id]
+}
