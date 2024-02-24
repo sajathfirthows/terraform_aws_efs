064aa1357015203d986b24d9446a966e651d7262
Sajath Firthows <106432527+sajathfirthows@users.noreply.github.com>
2024-02-25 00:34:57 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,19 @@
+variable "vpc_cidr" {
+  description = "10.0.0.0/16"
+}
+
+variable "subnet_1_cidr" {
+  description = "10.0.1.0/24"
+}
+
+variable "subnet_2_cidr" {
+  description = "10.0.2.0/24"
+}
+
+variable "az_1" {
+  description = "us-east-1a"
+}
+
+variable "az_2" {
+  description = "us-east-1b"
+}
